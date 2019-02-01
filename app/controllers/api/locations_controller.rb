dd1c5242cbfd8dca9f524e1f75889c7ce7201785
Safuh <Safoh95@windowslive.com>
2019-02-01 09:35:33 -0800

    heroku bug

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -1,16 +1,17 @@
 require "net/http"
-require 'IPinfo'
+# require 'IPinfo'
 class Api::LocationsController < ApplicationController
 
   def create
     access_token = '8ad192f4b825fa'
-    handler = IPinfo::create(access_token)
-    ip_address = Net::HTTP.get(URI("https://api.ipify.org"))
+    # handler = IPinfo::create(access_token)
+    # ip_address = Net::HTTP.get(URI("https://api.ipify.org"))
 
-    details = handler.details(ip_address)
-    @city = details.city # Emeryville
-    @loc = details.loc # 37.8342,-122.2900
-
+    # details = handler.details(ip_address)
+    # @city = details.city # Emeryville
+    # @loc = details.loc # 37.8342,-122.2900
+    @city = 'new city'
+    @lob = 'saf saf'
 
     render "api/location/show"
   end
