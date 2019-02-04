15bbec3136fb9142985451277557561430f087bc
Safuh <Safoh95@windowslive.com>
2019-02-04 15:02:20 -0800

    current loc

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -4,12 +4,31 @@ class Api::LocationsController < ApplicationController
 
   def create
     ip_address = Net::HTTP.get(URI("https://api.ipify.org"))
-    access_token = '8ad192f4b825fa'
-    handler = IPinfo::create(access_token)
+    # access_token = '8ad192f4b825fa'
+    # handler = IPinfo::create(access_token)
     
-    details = handler.details(ip_address)
-    @city = details.city # Emeryville
-    @loc = details.loc # 37.8342,-122.2900
+
+    location = Geocoder.search(ip_address)
+
+    innerhash = location[0].data
+
+    country = innerhash["country_name"]
+    state = innerhash["region_name"]
+    @city = innerhash["city"]
+    @loc = innerhash["loc"]
+    # zipcode = geoloc["zipcode"]
+    # latitude = geoloc["latitude"]
+    # longitude = geoloc["longitude"]
+    # area_code = geoloc["area_code"]
+
+    
+
+
+    # details = handler.details(ip_address)
+    # @city = details.city # Emeryville
+    # @loc = details.loc # 37.8342,-122.2900
+
+
     render "api/location/show"
   end
 
@@ -18,3 +37,5 @@ class Api::LocationsController < ApplicationController
   #   render 'api/location/show'
   # end
 end
+
+# request.env['REMOTE_ADDR']
